36588c40066505a9b5b9c9a1d6ebb0f27ef56fbe
Sathish kumar Ravi <Sathish kumar Ravi@DESKTOP-VLSES14>
2024-07-11 09:05:33 +0530

    second commit

---
+++ SeleniumProject/src/launch/NavigateToUrl.java
@@ -0,0 +1,10 @@
+package launch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateToUrl {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+}
+}
